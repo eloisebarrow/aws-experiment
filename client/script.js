dc389046bad80d77bbabff1b866414b5559aa066
eloisebarrow <eloisebarrow@gmail.com>
2020-01-18 19:13:30 -0500

    resolving rack <2.0.8 security vulnerability

--- client/script.js
+++ client/script.js
@@ -1,3 +1,5 @@
+const baseURL = 'http://localhost:3000/users';
+
 const button = document.querySelector("#button")
 
 button.addEventListener("click", async (e) => {
@@ -7,3 +9,15 @@ button.addEventListener("click", async (e) => {
   const resp = await axios.post("http://localhost:3000/users", userData);
   console.log(resp);
 });
+
+// let allImages = document.querySelectorAll('img');
+
+// const getImages = async () => {
+//   try {
+//     let resp = await axios.get(baseURL);
+//     console.log('this is resp', resp.data);
+//   } catch (e) {
+//     console.log(e.message)
+//   }
+// };
+
---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,26 @@
+Rails.application.config.to_prepare do
+  if defined?(ActiveStorage::Service::S3Service)
+    # Make ActiveStorage as public-read. @dinatih
+    # solution from https://github.com/rails/rails/issues/31419#issuecomment-370900013
+    ActiveStorage::Service::S3Service.class_eval do
+      def upload(key, io, checksum: nil)
+        instrument :upload, key: key, checksum: checksum do
+          begin
+            object_for(key)
+              .put(upload_options.merge(body: io, content_md5: checksum, acl: 'public-read'))
+          rescue Aws::S3::Errors::BadDigest
+            raise ActiveStorage::IntegrityError
+          end
+        end
+      end
+
+      def url(key, expires_in:, filename:, disposition:, content_type:)
+        instrument :url, key: key do |payload|
+          generated_url = object_for(key).public_url
+          payload[:url] = generated_url
+          generated_url
+        end
+      end
+    end
+  end
+end
